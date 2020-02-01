758a62c55672f8829456e43bbee773642914463d
Lakshmikanth9036 <kanth9036.1997@gmail.com>
2020-02-01 11:38:46 +0530

    [Lakshmikanth]solving problem using Stack DataStructure.solving balanced parenthesis problem using Stack

--- src/main/java/com/bridgelabz/DataStructurePrograms/BalancedParentheses.java
+++ src/main/java/com/bridgelabz/DataStructurePrograms/BalancedParentheses.java
@@ -1,10 +1,34 @@
 package com.bridgelabz.DataStructurePrograms;
 
+import com.bridgelabz.DataStructure.Stack;
+import com.bridgelabz.InputUtility.InputUtility;
+
 public class BalancedParentheses {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Stack<Character> stack = new Stack<>();
+		System.out.println("Enter the expression");
+		String s = InputUtility.getScanner().next();
+		int k = 0;
+		
+		while(k<s.length()){
+			char ch = s.charAt(k);
+			if(ch == '('){
+				stack.push(ch);
+			}
+			else if(ch == ')'){
+				if(!stack.isEmpty())
+					stack.pop();
+				if(stack.isEmpty()){
+					break;
+				}
+			}
+			k++;
+		}
+		if(stack.isEmpty() && k == s.length()-1)
+			System.out.println("Given expression is valid expression");
+		else
+			System.out.println("Given expression is not a valid expression");
 	}
 
 }
